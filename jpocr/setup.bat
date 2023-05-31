2852485f78edcc4bdd9bd843493d707db2296a64
xxh200812345 <46435236+xxh200812345@users.noreply.github.com>
2023-05-31 21:28:43 +0900

    2.1.1

--- jpocr/setup.bat
+++ jpocr/setup.bat
@@ -1,44 +1,42 @@
-chcp 65001 > nul
+REM Wait for user input of the Tesseract-OCR folder path without the final
+REM Example: E:\Program Files\Tesseract-OCR
+set /p "OCR_path=Please enter the Tesseract-OCR folder path:"
 
-REM 等待用户输入Tesseract-OCR的文件夹路径，不要最后带\
-REM 比如：E:\Program Files\Tesseract-OCR
-set /p "OCR_path=请输入Tesseract-OCR的文件夹路径："
+REM Print the input Tesseract-OCR folder path
+echo The path you entered is: %OCR_path%
 
-REM 打印输入的Tesseract-OCR的文件夹路径
-echo 你输入的路径是：%OCR_path%
-
-REM 跳转到当前文件的文件夹
+REM Jump to the current folder of this file
 pushd "%~dp0"
 
-REM 执行其他命令或操作
-echo 当前工作目录：%CD%
+REM Execute other commands or operations
+echo Current working directory: %CD%
 
-REM 自动设置白名单
-copy /Y "res\_my_word" "%OCR_path%\tessdata\configs\"
+REM Automatically set whitelist
+copy /Y "res\_my_word" "%OCR_path%\tessdata\configs"
 
 pause
 
-REM 设置自定义模型
+REM Set custom models
 
-REM 复制 num_1.traineddata 模型文件到 tessdata 文件夹
-copy /Y "jpocr\res\num_1.traineddata" "%OCR_path%\tessdata\"
+REM Copy num_1.traineddata model file to tessdata folder
+copy /Y "res\num_1.traineddata" "%OCR_path%\tessdata"
 
-REM 复制 eng.traineddata 模型文件到 tessdata 文件夹
-copy /Y "jpocr\res\eng.traineddata" "%OCR_path%\tessdata\"
+REM Copy eng.traineddata model file to tessdata folder
+copy /Y "res\eng.traineddata" "%OCR_path%\tessdata"
 
-REM 复制 jpn.traineddata 模型文件到 tessdata 文件夹
-copy /Y "jpocr\res\jpn.traineddata" "%OCR_path%\tessdata\"
+REM Copy jpn.traineddata model file to tessdata folder
+copy /Y "res\jpn.traineddata" "%OCR_path%\tessdata"
 
-REM 查看是否正确安装：`eng` `jpn` `num_1`
+REM Check if installed correctly: eng jpn num_1
 tesseract --list-langs
 
 pause
 
-REM 配置虚拟环境
+REM Configure virtual environment
 set VIRTUALENV_PATH=ai_jpocr_venv
 
 if exist %VIRTUALENV_PATH% (
-    echo Virtual environment already exists
+echo Virtual environment already exists
 ) else (
     echo Virtual environment does not exist, creating a new virtual environment...
 
@@ -49,7 +47,7 @@ call %VIRTUALENV_PATH%\Scripts\activate
 
 pip install -r requirements.txt
 
-REM 安装完毕
+REM Installation completed
 
 pause
 
