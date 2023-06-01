47d3920c5c182153204e806ec2ced75e17ae1093
xxh200812345 <46435236+xxh200812345@users.noreply.github.com>
2023-06-01 11:46:59 +0900

    2.1.2

--- jpocr/passport.py
+++ jpocr/passport.py
@@ -6,7 +6,7 @@ class Passport:
     def __init__(self, file_name):
         name, ext = os.path.splitext(file_name)
         # 护照文件名
-        self.file_name = file_name
+        self.file_name = name
         # PDF转PNG后保存文件名
         self.pdf2png_file_name = f"{name}_pdf2png.png"
         # 第一次裁剪后文件名
